03408111b45b35cb40cf46ff507e5ffde2110fea
AlessandroGCatino <catino.alessandrog@gmail.com>
2024-01-18 16:44:50 +0100

    fix: corretta data di domani instanziata e output

--- main.js
+++ main.js
@@ -1,19 +1,27 @@
 const output = document.getElementById("output")
 
-const lesson = new Date(2024, 1, 19, 9, 30, 0, 0);
-console.log(lesson)
+const lesson = new Date(2024, 0, 19, 9, 30, 0, 0);
 
 const clock = setInterval(countdown, 1000)
 
 function countdown(){
 
     let currentTime = new Date()
-    if(currentTime==lesson)
+    let remainingTime = Math.abs(lesson - currentTime)
+    if(lesson<currentTime)
     {
         output.innerHTML = "Lezione iniziata!"
         clearInterval(clock)
+
     } else {
-        output.innerHTML = currentTime.toLocaleTimeString()
+        // divido per 60 (minuti) * 1000ms
+        const ore = Math.floor(remainingTime / 3600000)
+        // divido per 60(minuti) * 1000 ms il resto delle ore
+        const minuti = Math.floor((remainingTime % 3600000) / 60000)
+        // divido per 1000ms il resto dei minuti
+        const secondi = Math.floor((remainingTime % 60000) / 1000)
+
+        output.innerHTML = `Mancano ${ore} ore, ${minuti} minuti, ${secondi} secondi alla lezione`
     }
 
 }
